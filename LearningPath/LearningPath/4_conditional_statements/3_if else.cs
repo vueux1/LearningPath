2d5c6d2138fd1f54f5dedaa817d9ccfc1830bb5b
alperen tekin <tekinalperen26@gmail.com>
2024-07-29 21:05:52 +0300

    if else

---
+++ LearningPath/LearningPath/4_conditional_statements/3_if else.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Ödev_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kullanıcı adınızı giriniz: ");
+            string kullaniciAdi = Console.ReadLine();
+
+            if (kullaniciAdi == "abc - 123 - 236 - cmk")
+            {
+                Console.WriteLine("Kullanıcı girişi başarılı");
+            }
+
+            else if (kullaniciAdi == "hhh - bbb - mmm")
+            {
+                Console.WriteLine("Kullanıcı girişiniz kitlenmiştir");
+            }
+
+            else { Console.WriteLine("Hatalı Kullanıcı Kodu"); }
+        }
+    }
+}
